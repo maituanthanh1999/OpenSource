a265601f26b0b6bddca577d621b18d5f8ecd0fd1
Mai Tuan Thanh <maituanthanh1999@gmai.com>
2021-08-21 15:32:46 +0700

    test

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char *ptr1=NULL, *ptr2=NULL;
+    char text[64];
+    ptr1 = (char *) malloc(3*sizeof(char));
+    printf("enter text: ");
+    fgets(ptr1,4,stdin);
+    puts(ptr1);
+    printf("length: %d-%d ",strlen(ptr1),sizeof(ptr1));
+    return 0;
+
+}
